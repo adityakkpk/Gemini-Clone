c168aedd60e9a197e402651ca600fd3c5cf17d09
Aditya Kumar Kushwaha <akkpk933@gmail.com>
2024-08-30 19:47:21 +0530

    UI created

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Sidebar from './Components/SideBar/Sidebar'
+import Main from './Components/Main/Main'
 
 const App = () => {
   return (
     <>
       <Sidebar />
+      <Main />
     </>
   )
 }
