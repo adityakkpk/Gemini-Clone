86c8404724dfa8fc301cd3afaa623a9f49c6dec6
Aditya Kumar Kushwaha <akkpk933@gmail.com>
2024-08-30 19:47:50 +0530

    UI Completed

--- src/assets/index.js
+++ src/assets/index.js
@@ -4,6 +4,13 @@ import message from "./Message.svg";
 import question from "./Question-mark.svg";
 import history from "./History.svg";
 import settings from "./Settings.svg";
+import compass from "./Compass.svg";
+import light from "./light.svg";
+import code from "./Code.svg";
+import gallery from "./gallery.svg"
+import mic from "./mic.svg"
+import send from "./send.svg"
+
 
 export {
     menuSVG,
@@ -11,5 +18,11 @@ export {
     message,
     question,
     history, 
-    settings
+    settings,
+    compass,
+    light,
+    code,
+    gallery,
+    mic,
+    send
 }
